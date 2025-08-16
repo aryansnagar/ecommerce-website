dac67894c9713298c65c6733d93f68c9b06e1b4b
Aryan Nagar <aryannagarco@gmail.com>
2025-08-16 16:09:52 +0530

    rebuild

--- frontend/logic/form.js
+++ frontend/logic/form.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         try {
             // Send to your local Node.js server
-            const response = await fetch("http://localhost:3000/save", {
+            const response = await fetch("https://ecommerce-website-5avb.onrender.com/save", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
